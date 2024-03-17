5ff6491cbc301311a5972fda7bc11ffdb4560c15
Wesley Deland <wesdeland@gmail.com>
2024-03-16 20:31:47 -0500

    module 8 done

--- 8/main.go
+++ 8/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(sumUp(numbers...))
+}
+
+func sumUp(numbers ...int) int {
+	sum := 0
+
+	for _, v := range numbers {
+		sum += v
+	}
+
+	return sum
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/price-calculator
+
+go 1.22.0
