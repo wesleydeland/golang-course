9f3f1121f4f28ced3591f60774c48ec2c420bf9e
Wesley Deland <wesdeland@gmail.com>
2024-03-17 21:32:11 -0500

    API WIP

---
+++ 11/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/rest-api/db"
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") //localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "event created successfully", "event": event})
+}
---
+++ 11/models/event.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserId      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	//later - save to db
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
